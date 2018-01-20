66d689259f47c6b93f39c7841074b0371a769cd8
Ali Hashim <ahashim@u.rochester.edu>
2018-01-19 23:26:20 -0500

    Initial Commit

---
+++ Face/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Face
+//
+//  Created by Ali Hashim on 1/19/18.
+//  Copyright © 2018 Ali Hashim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
